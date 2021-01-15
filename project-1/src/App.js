65fc83d2fe78f0c6d41e4b7137134e7a3399125b
Joel Brown <joel.anthony.brown1@gmail.com>
2021-01-14 20:46:54 -0500

    feat: adding functions to children 2 ways

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -13,32 +13,48 @@ class App extends Component {
     otherState: 'some other state',
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('the handle click button was clicked');
     // manipulate state to change a person's data
     this.setState({
       persons: [
         {name: 'Joel', age: 46, hobies: 'motorcycles' },
         {name: 'Desta', age: 38},
-        {name: 'Clarice', age: 22},
+        {name: newName, age: 22},
       ],
     })
+  }
+
+  changeNameHandler = (evt) => {
+    const newName = evt.target.value
+    this.setState({
+      persons: [
+        {name: 'Joel', age: 46, hobies: 'motorcycles' },
+        {name: 'Desta', age: 38},
+        {name: newName, age: 22},
+      ],
+    })
+
   }
 
   render() {
     return (
       <div className="App">
         <h1>This is a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch names</button>
-        {this.state.persons.map(p => {
+        <button onClick={this.switchNameHandler.bind(this, 'Clarice')}>Switch names</button>
+        {this.state.persons.map((p, i) => {
           console.log('this is the person: ', p);
-          return <Person name={p.name} age={p.age}> {p.hobies? p.hobies : null} </Person>
+          return (
+            <Person 
+              name={p.name} 
+              age={p.age} 
+              handleClick={(n) => this.switchNameHandler(n)}> 
+              {p.hobies? p.hobies : null} 
+              { i === this.state.persons.length - 1 ? <input type='text' onChange={this.changeNameHandler} value={p.name}/> : null}
+            </Person>
+            )
         })}
         <p>{this.state.otherState}</p>
-        {/* { this.renderPeople() } */}
-        {/* <Person name={'Joel'} age={45}>I like to ride motorcycles</Person>
-        <Person name={'Desta'} age={37}/>
-        <Person name={'Sam'} age={53}/> */}
       </div>
     );
   };
